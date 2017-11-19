c2606b3527ee84166e15641f86871062f2758fe3
Carlo Lucera <carlolucera@gmail.com>
2017-11-19 16:52:04 +0100

    First working version of WeatherService

--- app/src/main/java/com/gdgcatania/kotlinbootcamp/RetrofitService.kt
+++ app/src/main/java/com/gdgcatania/kotlinbootcamp/RetrofitService.kt
@@ -1,6 +1,11 @@
 package com.gdgcatania.kotlinbootcamp
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Retrofit Service builder used for getting enabling the use of the WheaterService
@@ -10,14 +15,33 @@ import retrofit2.Retrofit
  */
 
 class RetrofitService {
-    val openWeatherUrl = "api.openweathermap.org/data/2.5/"
 
     /**
      * return a @see WheaterService instance
      */
     fun createWeatherService(): WeatherService {
+
+        // interceptor for custom request (add API_KEY and Custom settings as query)
+
+        val interceptor = object : Interceptor{
+            override fun intercept(chain: Interceptor.Chain?): Response? {
+                val request = chain?.request();
+                val url = request?.url()?.newBuilder()
+                        ?.addQueryParameter("APPID", BuildConfig.API_KEY)
+                        ?.addQueryParameter("lang", "it")
+                        ?.addQueryParameter("unit", "metric")
+                        ?.build()
+                return chain?.proceed(request?.newBuilder()?.url(url)?.build())
+            }
+        }
+
+        // building OkHttpClient and add to retrofit builder
+
+        val weatherClient = OkHttpClient.Builder().addNetworkInterceptor(interceptor).build()
         val retrofit = Retrofit.Builder()
-                .baseUrl(openWeatherUrl)
+                .baseUrl(BuildConfig.ENDPOINT_URL)
+                .client(weatherClient)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
         return retrofit.create(WeatherService::class.java)
--- app/src/main/java/com/gdgcatania/kotlinbootcamp/WeatherService.kt
+++ app/src/main/java/com/gdgcatania/kotlinbootcamp/WeatherService.kt
@@ -1,5 +1,7 @@
 package com.gdgcatania.kotlinbootcamp
 
+import org.json.JSONObject
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,14 +15,58 @@ import retrofit2.http.Query
 
 interface WeatherService
 {
-    //TODO services call: current Wheater by cityName
+    //TODO change return type with the correct Data object
+
+    /**
+     * Current wheater for one location by city name
+     *
+     * param: query - the string for searching the locality
+     */
     @GET("weather")
-    fun getWheaterByCityName(@Query("q") query: String)
-    //TODO services call: current Wheater by cityId
+    fun getWeatherByCityName(@Query("q") query: String): Call<Any>
+
+    /**
+     * Current wheater for one location by City id
+     *
+     * param: id - the id of the city
+     */
+
     @GET("weather")
-    fun getWheaterByCityId(@Query("id") id: String)
-    //TODO services call: current Wheater by coordinate
+    fun getWeatherByCityId(@Query("id") id: Long): Call<Any>
+
+    /**
+     * Current wheater for one location defined by coordinates
+     *
+     * param: lat - latitude of the request wheater
+     * param: lng - longitude of the request wheater
+     */
     @GET("weather")
-    fun getWheaterByCityGeoPosition(@Query("lat") lat: String, @Query("lng") lng: String)
+    fun getWeatherByCityGeoPosition(@Query("lat") lat: Double, @Query("lng") lng: Double): Call<Any>
+
+    /**
+     * Current wheater for an area defined by the bounding box, the bounding box can be obtained
+     * using the Utility method getBoundingBox
+     *
+     * param: boundingBox - latitude of the request wheater
+     */
+
+    @GET("box/city")
+    fun getWeatherByBoundingBox(@Query("bbox") boundingBox: String): Call<Any>
+
+    /**
+     * Current wheater for a circular area defined by latitude, longitude and expeted number of cities
+     *
+     * param: lat - latitude of the request wheater
+     * param: lng - longitude of the request wheater
+     * param: cnt - number of cities around the point that should be returned
+     */
+
+    @GET("find")
+    fun getWeatherInCircle(@Query("lat") lat: Double, @Query("lng") lng: Double, @Query("cnt") cnt: Int): Call<Any>
 
 }
+
+/**
+ * Utility method used for parsing the bounding box for the @see getWeatherByBoundingBox method
+ */
+fun getBoundingBox(lat1: Double, lng1: Double, lat2: Double, lng2: Double, zoom: Double) = "$lat1,$lng1,$lat2,$lng2,$zoom"
